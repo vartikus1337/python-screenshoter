8a8370707a14651f78cbb6172db60a1c45df7c96
vartikus1337 <pasha_klevtsov@vk.com>
2023-02-23 13:32:45 +0300

    #2 create im main test functions add in folder Token for yadisk

--- main.py
+++ main.py
@@ -1,2 +1,55 @@
+from Token import TOKEN
+import yadisk
+import pyautogui
+import os
+from datetime import date, datetime
+from time import sleep
+
+now = datetime.now()
+today = str(date.today())
+minutes = 1
+
+
+def update_time():
+    global now
+    global today
+    now = datetime.now()
+    today = str(date.today())
+
+
+def create_folder():
+    update_time()
+    if not os.path.exists(today):
+        os.mkdir(today)
+
+
+def create_screen():
+    update_time()
+    screen = pyautogui.screenshot()
+    screen.save(f'{today}/{now.hour}-{now.minute}-{now.second}.png')
+
+
+def test_screenshots():
+    count = 0
+    create_folder()
+    while count < 3:
+        create_folder()
+        sleep(10)
+        create_screen()
+        count += 1
+
+
+def test_yadisk():
+    y = yadisk.YaDisk(token=TOKEN)
+    print(y.check_token())
+
+    y.mkdir(f"/{today}")
+
+
+def test_write_files_for_upload():
+    content = os.listdir(f'{today}/')
+    print(content)
+
+
 if __name__ == "__main__":
-    print("Hello World!")
+    test_screenshots()
