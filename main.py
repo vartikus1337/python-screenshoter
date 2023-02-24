948564d21dcc47ac82a1e596bf9822145ab803d9
vartikus1337 <pasha_klevtsov@vk.com>
2023-02-24 13:28:30 +0300

    #7 Last changes

--- main.py
+++ main.py
@@ -23,39 +23,45 @@ def update_time():
     global today
     now = datetime.now()
     today = str(date.today())
+    print("Update time")
 
 
 def create_folder():
     update_time()
     if not os.path.exists(today):
         os.mkdir(today)
+        print("Create folder")
 
 
 def create_screen():
     update_time()
     screen = pyautogui.screenshot()
     screen.save(f'{today}/{now.hour}-{now.minute}.png')
+    print("Create screen")
+
+
+def upload_on_disk():
+    if not disk.exists(f"/{today}"):
+        disk.mkdir(f"/{today}")
+        print("Created folder in yadisk")
+    for img in os.listdir(f'{today}/'):
+        if img not in get_images():
+            disk.upload(f"{today}/" + img, f"{today}/" + img)
+            print(f"upload {img}")
 
 
 def main():
     count = 0
-    create_folder()
+    create_folder()     
     while True:
         create_folder()
+        print("waiting minutes_for_screen_shot")
         sleep(minutes_for_screen_shot * 60)
         create_screen()
         if count == minutes_for_upload_images:
             upload_on_disk()
             count = 0
         count += 1
-
-
-def upload_on_disk():
-    if not disk.exists(f"/{today}"):
-        disk.mkdir(f"/{today}")
-    for img in os.listdir(f'{today}/'):
-        if img not in get_images():
-            disk.upload(f"{today}/" + img, f"{today}/" + img)
 
 
 if __name__ == "__main__":
